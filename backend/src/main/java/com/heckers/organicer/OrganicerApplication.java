86cc75bfa9f3cd3d62bed9f1a64fffb00ef28d9e
Till Nitsche <till.nitsche@web.de>
2024-03-03 21:43:51 +0100

    init project setup

---
+++ backend/src/main/java/com/heckers/organicer/OrganicerApplication.java
@@ -0,0 +1,13 @@
+package com.heckers.organicer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganicerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganicerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/heckers/organicer/OrganicerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heckers.organicer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganicerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
