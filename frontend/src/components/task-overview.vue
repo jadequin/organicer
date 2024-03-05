4754aa3402e37c56dc754f1c8f982bdd1d605c71
Liv Madeleine Nitsche <liv.madeleine.nitsche@mni.thm.de>
2024-03-06 00:02:45 +0100

    Hab die tasks rund gemacht und eingabefelder hinzugefügt

--- frontend/src/components/task-overview.vue
+++ frontend/src/components/task-overview.vue
@@ -9,22 +9,43 @@ export default {
   <div>
     <h2>TaskList</h2>
     <ul class="list-group">
-      <li class="list-group-item">
-        <input class="form-check-input me-1" type="checkbox" value="" id="firstCheckboxStretched">
-        <label class="form-check-label stretched-link" for="firstCheckboxStretched">First checkbox</label>
+      <li class="list-group-item task-item">
+        <div class="custom-checkbox d-flex align-items-center">
+          <input class="form-check-input me-1 custom-checkbox-input" type="checkbox" value="" id="firstCheckboxStretched">
+          <input class="form-control" type="text" placeholder="Bezeichnung der Aufgabe">
+        </div>
       </li>
-      <li class="list-group-item">
-        <input class="form-check-input me-1" type="checkbox" value="" id="secondCheckboxStretched">
-        <label class="form-check-label stretched-link" for="secondCheckboxStretched">Second checkbox</label>
+      <li class="list-group-item task-item">
+        <div class="custom-checkbox d-flex align-items-center">
+          <input class="form-check-input me-1 custom-checkbox-input" type="checkbox" value="" id="secondCheckboxStretched">
+          <input class="form-control" type="text" placeholder="Bezeichnung der Aufgabe">
+        </div>
       </li>
-      <li class="list-group-item">
-        <input class="form-check-input me-1" type="checkbox" value="" id="thirdCheckboxStretched">
-        <label class="form-check-label stretched-link" for="thirdCheckboxStretched">Third checkbox</label>
+      <li class="list-group-item task-item">
+        <div class="custom-checkbox d-flex align-items-center">
+          <input class="form-check-input me-1 custom-checkbox-input" type="checkbox" value="" id="thirdCheckboxStretched">
+          <input class="form-control" type="text" placeholder="Bezeichnung der Aufgabe">
+        </div>
       </li>
     </ul>
   </div>
 </template>
 
 <style scoped>
+
+.custom-checkbox-input {
+  width: 30px;
+  height: 30px;
+  cursor: pointer;
+}
+
+
+.task-item {
+  margin: 2px;
+  border-radius: 10px;
+}
+.form-control {
+  margin: 5px;
+}
 
 </style>
